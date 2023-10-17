b9dc0a4e62dd9ced0bb0d4b155481ec305246c82
dfface <dfface@sina.com>
2023-10-18 00:21:17 +0800

    feat: vite server

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  // 配置前端服务地址和端口
+  server: {
+    host: '0.0.0.0',
+    port: 8080,
+    // 是否开启 https
+    https: false,
+  },
   test: {
     watch: false
   }
